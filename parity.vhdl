aa9a6a50a63b464c3aa2a6000dab508fe27d2f08
Chris Cummins <chrisc.101@gmail.com>
2013-09-21 17:58:33 +0100

    Add .gitignore

--- parity.vhdl
+++ parity.vhdl
@@ -1,3 +1,9 @@
+ --
+ -- Parity generator.
+ -- Taken from VHDL for Logic Synthesis (3rd edition), section 3.10, p34.
+ -- Chris Cummins - 21/9/13
+ --
+
 entity parity is
   port (d7, d6, d5, d4, d3, d2, d1, d0 : in bit;
         mode : in bit;
