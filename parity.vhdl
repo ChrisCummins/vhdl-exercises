f8cf24336a5ff3dc5b982c6cd590e9e997b7db0d
Chris Cummins <chrisc.101@gmail.com>
2013-09-21 17:54:41 +0100

    Add parity generator
    Follows the worked example from VHDL for Logic Synthesis, section 3.10, p34.

---
+++ parity.vhdl
@@ -0,0 +1,12 @@
+entity parity is
+  port (d7, d6, d5, d4, d3, d2, d1, d0 : in bit;
+        mode : in bit;
+        result : out bit);
+end;
+
+architecture behaviour of parity is
+  signal sum : bit;
+begin
+    sum <= d0 xor d1 xor d2 xor d3 xor d4 xor d5 xor d6 xor d7;
+    result <= sum when mode = '1' else not sum;
+end;
