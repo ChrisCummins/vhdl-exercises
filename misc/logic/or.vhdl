b03eaca9523ac07bf360030eff31021a80eea0bf
Chris Cummins <chrisc.101@gmail.com>
2013-09-23 01:18:22 +0100

    misc/logic: Add 4 pin or gate

--- misc/logic/or.vhdl
+++ misc/logic/or.vhdl
@@ -10,13 +10,29 @@ begin
 end architecture behaviour;
 
 
+entity logic_or4 is
+  port (a, b, c, d : in bit;
+        q : out bit);
+end entity logic_or4;
+
+architecture behaviour of logic_or4 is
+  signal a1, b1 : bit;
+begin
+  or1 : entity work.logic_or(behaviour)
+    port map (a, b, a1);
+  or2 : entity work.logic_or(behaviour)
+    port map (c, d, b1);
+  or3 : entity work.logic_or(behaviour)
+    port map (a1, b1, q);
+end architecture behaviour;
+
+
 
 --
 -- TEST BENCH
 --
 
 entity or_tb is
-begin
 end;
 
 architecture tests of or_tb is
@@ -32,3 +48,33 @@ begin
       wait;
     end process stimulus;
 end;
+
+entity or4_tb is
+end;
+
+architecture tests of or4_tb is
+  signal a, b, c, d, q : bit;
+begin
+  dut : entity work.logic_or4(behaviour)
+    port map (a, b, c, d, q);
+  stimulus : process is
+    begin
+      a <= '0'; b <= '0'; c <= '0'; d <= '0'; wait for 20 ns;
+      a <= '0'; b <= '0'; c <= '0'; d <= '1'; wait for 20 ns;
+      a <= '0'; b <= '0'; c <= '1'; d <= '0'; wait for 20 ns;
+      a <= '0'; b <= '0'; c <= '1'; d <= '1'; wait for 20 ns;
+      a <= '0'; b <= '1'; c <= '0'; d <= '0'; wait for 20 ns;
+      a <= '0'; b <= '1'; c <= '0'; d <= '1'; wait for 20 ns;
+      a <= '0'; b <= '1'; c <= '1'; d <= '0'; wait for 20 ns;
+      a <= '0'; b <= '1'; c <= '1'; d <= '1'; wait for 20 ns;
+      a <= '1'; b <= '0'; c <= '0'; d <= '0'; wait for 20 ns;
+      a <= '1'; b <= '0'; c <= '0'; d <= '1'; wait for 20 ns;
+      a <= '1'; b <= '0'; c <= '1'; d <= '0'; wait for 20 ns;
+      a <= '1'; b <= '0'; c <= '1'; d <= '1'; wait for 20 ns;
+      a <= '1'; b <= '1'; c <= '0'; d <= '0'; wait for 20 ns;
+      a <= '1'; b <= '1'; c <= '0'; d <= '1'; wait for 20 ns;
+      a <= '1'; b <= '1'; c <= '1'; d <= '0'; wait for 20 ns;
+      a <= '1'; b <= '1'; c <= '1'; d <= '1'; wait for 20 ns;
+      wait;
+    end process stimulus;
+ end architecture tests;
