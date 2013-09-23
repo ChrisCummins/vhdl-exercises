334c2af929dec89c63d1839e966fd81edf839ca0
Chris Cummins <chrisc.101@gmail.com>
2013-09-23 01:14:35 +0100

    misc/logic: Add 4 pin and gate

--- misc/logic/and.vhdl
+++ misc/logic/and.vhdl
@@ -10,13 +10,28 @@ begin
 end architecture behaviour;
 
 
+entity logic_and4 is
+  port (a, b, c, d : in bit;
+        q : out bit);
+begin
+end entity logic_and4;
+
+architecture behaviour of logic_and4 is
+  signal a1, b1 : bit;
+begin
+  and1 : entity work.logic_and(behaviour)
+    port map (a, b, a1);
+  and2 : entity work.logic_and(behaviour)
+    port map (c, d, b1);
+  and3 : entity work.logic_and(behaviour)
+    port map (a1, b1, q);
+end architecture behaviour;
 
 --
 -- TEST BENCH
 --
 
 entity and_tb is
-begin
 end;
 
 architecture tests of and_tb is
@@ -32,3 +47,33 @@ begin
       wait;
     end process stimulus;
 end;
+
+entity and4_tb is
+end;
+
+architecture tests of and4_tb is
+  signal a, b, c, d, q : bit;
+begin
+  dut : entity work.logic_and4(behaviour)
+    port map (a, b, c, d, q);
+  stimulus : process is
+    begin
+      a <= '0'; b <= '0'; c <= '0'; d <= '0'; wait for 20 ns;
+      a <= '0'; b <= '0'; c <= '0'; d <= '1'; wait for 20 ns;
+      a <= '0'; b <= '0'; c <= '1'; d <= '0'; wait for 20 ns;
+      a <= '0'; b <= '0'; c <= '1'; d <= '1'; wait for 20 ns;
+      a <= '0'; b <= '1'; c <= '0'; d <= '0'; wait for 20 ns;
+      a <= '0'; b <= '1'; c <= '0'; d <= '1'; wait for 20 ns;
+      a <= '0'; b <= '1'; c <= '1'; d <= '0'; wait for 20 ns;
+      a <= '0'; b <= '1'; c <= '1'; d <= '1'; wait for 20 ns;
+      a <= '1'; b <= '0'; c <= '0'; d <= '0'; wait for 20 ns;
+      a <= '1'; b <= '0'; c <= '0'; d <= '1'; wait for 20 ns;
+      a <= '1'; b <= '0'; c <= '1'; d <= '0'; wait for 20 ns;
+      a <= '1'; b <= '0'; c <= '1'; d <= '1'; wait for 20 ns;
+      a <= '1'; b <= '1'; c <= '0'; d <= '0'; wait for 20 ns;
+      a <= '1'; b <= '1'; c <= '0'; d <= '1'; wait for 20 ns;
+      a <= '1'; b <= '1'; c <= '1'; d <= '0'; wait for 20 ns;
+      a <= '1'; b <= '1'; c <= '1'; d <= '1'; wait for 20 ns;
+      wait;
+    end process stimulus;
+end architecture tests;
