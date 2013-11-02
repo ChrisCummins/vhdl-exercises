12ff5e29eb5c302b56ee0988ffe114792d0ef8bd
Chris Cummins <chrisc.101@gmail.com>
2013-11-02 20:09:18 +0000

    rc/msf_bits: Add implementation

--- ee3dsd/Radio_Clock/msf_bits.vhd
+++ ee3dsd/Radio_Clock/msf_bits.vhd
@@ -27,12 +27,78 @@ entity msf_bits is
 end msf_bits;
 
 architecture rtl of msf_bits is
+  -- We use an intermediate signal rather than writing directly to the port:
+  signal   tr_var:      std_logic := '0';
 
-  -- Your declarations go here --
+  -- This contains the data input from the last clock cycle:
+  signal   di_var:      byte      := byte_null;
 
+  -- This keeps track of whether we're currently on a high or low pulse:
+  signal   pulse:       std_logic := '0';
+
+  -- This keeps track of whether we're currently sampling a pulse:
+  signal   sampling:    bit       := '0';
+
+  -- The number of clock cycles after a second begins that we decide on the bit
+  -- values. SAMPLE1 is the first bit and SAMPLE2 is the second:
+  constant SAMPLE1_TIME: natural   := clk_freq * 150 / 1000;
+  constant SAMPLE2_TIME: natural   := clk_freq * 250 / 1000;
+
+  -- We output both bits at the same time, so need to store the first bit while
+  -- the second is being sampled:
+  signal   bao_var:     std_logic  := '0';
+
+  -- The number of clock cycles since the start of a second:
+  signal   s_count:     natural range 0 to SAMPLE2_TIME + 1;
 begin
 
-  -- Your implementation goes here --
+  process(clk,rst)
+  begin
+
+    if rst = '1' then                       -- Reset everything
+      sampling <= '0';
+      s_count <= 0;
+      bao <= '0' after gate_delay;
+      bbo <= '0' after gate_delay;
+      tr <= '0' after gate_delay;
+    elsif clk'event and clk = '1' then
+      tr_var <= '0';                        -- Zero the trigger
+
+      if di > di_var then                   -- Check for rising or falling edges
+        pulse <= '1';
+      elsif di < di_var then
+        pulse <= '0';
+      end if;
+
+      if si = '1' then                      -- Check for a second-in pulse
+        sampling <= '1';
+      end if;
+
+      if sampling = '1' then                -- Bump our clock counter
+        s_count <= s_count + 1;
+
+        -- if we're currently sampling a pulse and we've reached the sample
+        -- time for either bit, then make a decision on whether the bit is a
+        -- high or low and trigger a new bit out when both bits have been
+        -- sampled:
+        if s_count = SAMPLE1_TIME then
+          bao_var <= pulse;
+        elsif s_count = SAMPLE2_TIME then
+          sampling <= '0';
+          s_count <= 0;
+
+          tr_var <= '1';
+
+          bao <= bao_var after gate_delay;
+          bbo <= pulse after gate_delay;
+        end if;
+      end if;
+
+      di_var <= di;                         -- Store the current input
+      tr <= tr_var after gate_delay;        -- Set our trigger output
+    end if;
+
+  end process;
 
 end rtl;
 
