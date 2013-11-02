8808ee57dea6102d3f611bb3f8769039100df45b
Chris Cummins <chrisc.101@gmail.com>
2013-11-02 18:41:27 +0000

    rc/dcf_bits: Add testbench

--- ee3dsd/Radio_Clock/dcf_bits.vhd
+++ ee3dsd/Radio_Clock/dcf_bits.vhd
@@ -34,3 +34,63 @@ begin
   -- Your implementation goes here --
 
 end rtl;
+
+------ END OF DCF_SYNC ------
+
+--
+-- Test bench
+--
+library IEEE;
+
+use IEEE.numeric_std.all;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_1164.all;
+use STD.textio.all;
+use WORK.std_logic_textio.all;
+use WORK.util.all;
+
+entity dcf_bits_tb is
+    generic (clk_freq: positive := 100); -- 100 Hz
+end dcf_bits_tb;
+
+architecture tests of dcf_bits_tb is
+  signal rst: std_logic := '0';
+  signal clk: std_logic := '0';
+  signal di:  byte      := byte_unknown;
+
+  signal so:  std_logic := 'X';
+  signal mo:  std_logic := 'X';
+  signal bo:  std_logic := 'X';
+  signal tr:  std_logic := 'X';
+begin
+  sync: entity WORK.dcf_sync(rtl)
+    port map (rst, clk, di, so, mo);
+  bits: entity WORK.dcf_bits(rtl)
+    port map (rst, clk, di, so, bo, tr);
+  process is
+    constant clk_period: time := 1000 ms / clk_freq;
+
+    file     data:       text;
+    variable data_line:  line;
+
+    variable clk_var:    std_logic;
+    variable di_var:     byte;
+  begin
+
+    file_open(data, "../cw/cw2/dcf_sync_tb-stimulus.txt", read_mode);
+
+    while not endfile(data) loop
+      readline(data, data_line);
+      read(data_line, clk_var);
+      read(data_line, di_var);
+
+      clk <= clk_var;
+      di  <= di_var;
+
+      wait for clk_period / 2;
+    end loop;
+
+    file_close(data);
+    wait;
+  end process;
+end tests;
