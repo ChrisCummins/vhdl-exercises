818663cfde46aefcf8bba3394d94f794931bd0ed
Chris Cummins <chrisc.101@gmail.com>
2013-11-02 15:30:49 +0000

    rc/msf_sync: Add testbench

--- ee3dsd/Radio_Clock/msf_sync.vhd
+++ ee3dsd/Radio_Clock/msf_sync.vhd
@@ -33,3 +33,59 @@ begin
   -- Your implementation goes here --
 
 end rtl;
+
+------ END OF MSF_SYNC ------
+
+--
+-- Test bench
+--
+library IEEE;
+
+use IEEE.numeric_std.all;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_1164.all;
+use STD.textio.all;
+use WORK.std_logic_textio.all;
+use WORK.util.all;
+
+entity msf_sync_tb is
+    generic (clk_freq: positive := 100); -- 100 Hz
+end msf_sync_tb;
+
+architecture tests of msf_sync_tb is
+  signal rst: std_logic := '0';
+  signal clk: std_logic := '0';
+  signal di:  byte      := byte_unknown;
+
+  signal so:  std_logic := 'X';
+  signal mo:  std_logic := 'X';
+begin
+  dut: entity work.msf_sync(rtl)
+    port map (rst, clk, di, so, mo);
+  process is
+    constant clk_period: time := 1000 ms / clk_freq;
+
+    file     data:       text;
+    variable data_line:  line;
+
+    variable clk_var:    std_logic;
+    variable di_var:     byte;
+  begin
+
+    file_open(data, "../cw/cw2/msf_sync_tb-stimulus.txt", read_mode);
+
+    while not endfile(data) loop
+      readline(data, data_line);
+      read(data_line, clk_var);
+      read(data_line, di_var);
+
+      clk <= clk_var;
+      di  <= di_var;
+
+      wait for clk_period / 2;
+    end loop;
+
+    file_close(data);
+    wait;
+  end process;
+end tests;
