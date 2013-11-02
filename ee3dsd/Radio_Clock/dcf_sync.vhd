045fb300707daf333c561f60f2c0513b9226af43
Chris Cummins <chrisc.101@gmail.com>
2013-11-02 14:17:00 +0000

    rc/dcf_signal: Style fixes and alignments

--- ee3dsd/Radio_Clock/dcf_sync.vhd
+++ ee3dsd/Radio_Clock/dcf_sync.vhd
@@ -8,35 +8,35 @@ entity dcf_sync is
 
     generic
     (
-        clk_freq:   positive := 100; -- Hz
-        gate_delay: time     := 1 ns
+        clk_freq   : positive := 100; -- Hz
+        gate_delay : time     := 1 ns
     );
 
     port
     (
-        rst: in  std_logic := 'X';          -- Reset
-        clk: in  std_logic := 'X';          -- Clock
+        rst : in  std_logic   := 'X';          -- Reset
+        clk : in  std_logic   := 'X';          -- Clock
 
-        di:  in  byte      := byte_unknown; -- Data in
-        so:  out std_logic := '0';          -- Start of second
-        mo:  out std_logic := '0'           -- Start of minute
+        di  : in  byte        := byte_unknown; -- Data in
+        so  : out std_logic   := '0';          -- Start of second
+        mo  : out std_logic   := '0'           -- Start of minute
     );
 
 end dcf_sync;
 
 architecture rtl of dcf_sync is
-  signal so_var : std_logic := '0';         -- so port var
-  signal mo_var : std_logic := '0';         -- mo port var
-  signal di_var : byte := byte_null;        -- Last di sampled
+  signal so_var         : std_logic := '0';                      -- so port var
+  signal mo_var         : std_logic := '0';                      -- mo port var
+  signal di_var         : byte      := byte_null;                -- Last di sampled
 
-  constant MIN_S_TIME   : natural := clk_freq - clk_freq / 5;  -- Min time between second pulses
-  constant MAX_S_TIME   : natural := clk_freq + clk_freq / 10; -- Max time between second pulses
-  constant RESET_S_TIME : natural := clk_freq * 3;             -- Max time to wait before resetting
-  signal s_count : natural range 0 to RESET_S_TIME + 1; -- The counter
+  constant MIN_S_TIME   : natural   := clk_freq - clk_freq / 5;  -- Min time between second pulses
+  constant MAX_S_TIME   : natural   := clk_freq + clk_freq / 10; -- Max time between second pulses
+  constant RESET_S_TIME : natural   := clk_freq * 3;             -- Max time to wait before resetting
+  signal s_count        : natural range 0 to RESET_S_TIME + 1;
 
-  constant M_UNINIT     : natural := 62;    -- Value for uninitialised state
-  constant M_PART_INIT  : natural := 61;    -- Value for partially initialised state
-  signal m_count : natural range 0 to M_UNINIT := M_UNINIT; -- The counter
+  constant M_UNINIT     : natural   := 62;                       -- Value for uninitialised state
+  constant M_PART_INIT  : natural   := 61;                       -- Value for partially initialised state
+  signal m_count        : natural range 0 to M_UNINIT := M_UNINIT;
 begin
   process(clk, rst)
   begin
@@ -44,7 +44,8 @@ begin
       so <= '0' after gate_delay;           -- Reset the outputs
       mo <= '0' after gate_delay;
     elsif clk'event and clk = '1' then
-      so_var <= '0'; mo_var <= '0';         -- Zero the outputs
+      so_var  <= '0';                       -- Zero the outputs
+      mo_var  <= '0';
       s_count <= s_count + 1;               -- Bump the clock counter
 
       -- Look for rising edges, either because we're expecting a second, or
@@ -53,13 +54,13 @@ begin
       if (di > di_var and s_count > MIN_S_TIME and s_count < MAX_S_TIME)
         or (di > di_var and m_count = M_UNINIT) then
         s_count <= 0;                       -- Reset clock counter
-        so_var <= '1';                      -- Output second pulse
+        so_var  <= '1';                     -- Output second pulse
 
         if m_count < 60 then
           m_count <= m_count + 1;           -- Count another second
         elsif m_count = 60 then
           m_count <= 0;                     -- Reset the minute counter
-          mo_var <= '1';                    -- Output start of minute pulse
+          mo_var  <= '1';                   -- Output start of minute pulse
         elsif m_count = M_UNINIT then
           -- We've now in a partially-initialised state, i.e. we've found our
           -- first second to latch onto but we haven't received a full minute
@@ -74,7 +75,7 @@ begin
       elsif (m_count = 59 and s_count = clk_freq)
         or (m_count = M_PART_INIT and s_count > MAX_S_TIME) then
         s_count <= 0;                       -- Reset clock counter
-        so_var <= '1';                      -- Add in missing second pulse
+        so_var  <= '1';                     -- Add in missing second pulse
         m_count <= 60;
 
       -- This is our 'false start' check. If we reach this point, it's either
@@ -88,8 +89,8 @@ begin
       end if;
 
       di_var <= di;                         -- Save di for next time
-      so <= so_var after gate_delay;        -- Set our outputs
-      mo <= mo_var after gate_delay;
+      so     <= so_var after gate_delay;    -- Set our outputs
+      mo     <= mo_var after gate_delay;
     end if;
   end process;
 end rtl;
@@ -113,23 +114,23 @@ entity dcf_sync_tb is
 end dcf_sync_tb;
 
 architecture tests of dcf_sync_tb is
-  signal rst:     std_logic := '0';
-  signal clk:     std_logic := '0';
-  signal di:      byte      := byte_unknown;
+  signal rst : std_logic := '0';
+  signal clk : std_logic := '0';
+  signal di  : byte      := byte_unknown;
 
-  signal so:      std_logic := 'X';
-  signal mo:      std_logic := 'X';
+  signal so  : std_logic := 'X';
+  signal mo  : std_logic := 'X';
 begin
   dut: entity work.dcf_sync(rtl)
     port map (rst, clk, di, so, mo);
   process is
     constant clk_period : time := 10 ms; -- 100 Hz
 
-    file     data:      text;
-    variable data_line: line;
+    file     data       : text;
+    variable data_line  : line;
 
-    variable clk_var:   std_logic;
-    variable di_var:    byte;
+    variable clk_var    : std_logic;
+    variable di_var     : byte;
   begin
 
     file_open(data, "../cw/cw2/tb-stimulus.txt", read_mode);
@@ -140,7 +141,7 @@ begin
       read(data_line, di_var);
 
       clk <= clk_var;
-      di <= di_var;
+      di  <= di_var;
 
       wait for clk_period / 2;
     end loop;
