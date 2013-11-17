a1e516081a7595a60fbb4d1c29e10fe1c57ced07
Chris Cummins <chrisc.101@gmail.com>
2013-11-17 20:21:28 +0000

    rc: Add {dcf,msf}_bits testbenches

---
+++ ee3dsd/Radio_Clock/msf_bits_testbench.vhd
@@ -0,0 +1,57 @@
+library IEEE;
+
+use IEEE.numeric_std.all;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_1164.all;
+use STD.textio.all;
+use WORK.std_logic_textio.all;
+use WORK.util.all;
+
+entity msf_bits_testbench is
+    generic (clk_freq: positive := 100); -- 100 Hz
+end msf_bits_testbench;
+
+architecture tests of msf_bits_testbench is
+  signal rst: std_logic := '0';
+  signal clk: std_logic := '0';
+  signal di:  byte      := byte_unknown;
+
+  signal so:  std_logic := 'X';
+  signal mo:  std_logic := 'X';
+  signal bao: std_logic := 'X';
+  signal bbo: std_logic := 'X';
+  signal tr:  std_logic := 'X';
+begin
+  sync: entity WORK.msf_sync(rtl)
+    generic map (clk_freq => clk_freq)
+    port map (rst, clk, di, so, mo);
+  bits: entity WORK.msf_bits(rtl)
+    generic map (clk_freq => clk_freq)
+    port map (rst, clk, di, so, bao, bbo, tr);
+  process is
+    constant clk_period: time := 1000 ms / clk_freq;
+
+    file     data:       text;
+    variable data_line:  line;
+
+    variable clk_var:    std_logic;
+    variable di_var:     byte;
+  begin
+
+    file_open(data, "../cw/cw2/msf_sync_tb-stimulus.txt", read_mode);
+
+    while not endfile(data) loop
+      readline(data, data_line);
+      read(data_line, clk_var);
+      read(data_line, di_var);
+
+      clk <= clk_var;
+      di  <= di_var;
+
+      wait for clk_period / 2;
+    end loop;
+
+    file_close(data);
+    wait;
+  end process;
+end tests;
