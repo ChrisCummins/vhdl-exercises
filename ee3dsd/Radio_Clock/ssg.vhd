d732f4e1deb262c59ede1c862cf8ea5aa80763e1
Chris Cummins <chrisc.101@gmail.com>
2013-11-19 13:36:03 +0000

    rc: Rename ssg_tb to ssg_testbench

--- ee3dsd/Radio_Clock/ssg.vhd
+++ ee3dsd/Radio_Clock/ssg.vhd
@@ -44,11 +44,20 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use WORK.util.all;
 
-entity ssg_tb is
-  generic (clk_freq: positive := 100); -- Hz
-end ssg_tb;
+entity ssg_testbench is
 
-architecture tests of ssg_tb is
+  generic
+  (
+      clk_freq: positive := 100 -- Hz
+  );
+
+end ssg_testbench;
+
+architecture tests of ssg_testbench is
+
+  constant test_duration: time      := 10000 ms;
+  constant clk_period:    time      := 1000 ms / clk_freq;
+  signal end_flag:        std_logic := '0';
 
   signal clk: std_logic;
   signal wr: std_logic;
@@ -59,7 +68,40 @@ architecture tests of ssg_tb is
 begin
 
   dut: entity work.ssg(behav)
-    generic map (clk_freq => clk_freq)
-    port map (clk, wr, di, an, ka);
+    generic map
+    (
+        clk_freq => clk_freq
+    )
+    port map
+    (
+         clk     => clk,
+         wr      => wr,
+         di      => di,
+         an      => an,
+         ka      => ka
+    );
+
+  process -- Process to end test after duration
+  begin
+
+    wait for test_duration;
+    end_flag <= '1';
+
+    wait;
+  end process;
+
+  process is -- Process to set 'clk'
+    variable clk_var: std_logic := '0';
+  begin
+
+    while (end_flag = '0') loop
+      clk <= '0';
+      wait for clk_period / 2;
+      clk <= '1';
+      wait for clk_period / 2;
+    end loop;
+
+    wait;
+  end process;
 
 end architecture;
