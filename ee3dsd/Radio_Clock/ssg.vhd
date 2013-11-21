98b1056e70a60c9b2afc3d7489fab561d62c5de5
Chris Cummins <chrisc.101@gmail.com>
2013-11-21 15:43:03 +0000

    rc/ssg: Refactor for 2 processes (always write)

--- ee3dsd/Radio_Clock/ssg.vhd
+++ ee3dsd/Radio_Clock/ssg.vhd
@@ -27,13 +27,12 @@ architecture behav of ssg is
 
   constant wait_period: natural := clk_freq * 4 / 1000000;
 
-  type    states  is (st_idle, st_write, st_wait);
+  type    states  is (st_write, st_wait);
   subtype index   is natural range 3 downto 0;
   subtype counter is natural range wait_period + 1 downto 0;
 
   signal state:         states                  := st_wait;
   signal next_state:    states                  := st_wait;
-  signal wr_sampled:    std_logic               := '0';
   signal di_sampled:    byte_vector(3 downto 0) := (others => byte_zero);
 
   signal an_index:      index                   := 0;
@@ -49,44 +48,40 @@ begin
 
     if clk'event and (clk = '1') then
 
-      wr_sampled     <= wr              after gate_delay;
-      di_sampled     <= di              after gate_delay;
-      state          <= next_state      after gate_delay;
-      an_index       <= next_an_index   after gate_delay;
-      cnt            <= next_cnt        after gate_delay;
+      di_sampled     <= di                      after gate_delay;
+      state          <= next_state              after gate_delay;
+      an_index       <= next_an_index           after gate_delay;
+      cnt            <= next_cnt                after gate_delay;
+
+      if (wr = '1') then
+
+        di_sampled   <= di_sampled              after gate_delay;
+
+      end if;
 
     end if;
 
   end process;
 
-  process (wr_sampled, di_sampled, state)
+  process (di_sampled, state)
   begin
 
     case state is
-
-      when st_idle =>
-
-        if (wr_sampled = '1') then
-          next_state    <= st_write        after gate_delay;
-        else
-          next_state    <= state           after gate_delay;
-        end if;
 
       when st_write =>
 
         an              <= (others => '1')      after gate_delay;
         an(an_index)    <= '0'                  after gate_delay;
         ka              <= di_sampled(an_index) after gate_delay;
+        next_state      <= st_wait              after gate_delay;
 
         if (an_index = 3) then
 
           next_an_index <= 0                    after gate_delay;
-          next_state    <= st_idle              after gate_delay;
 
         else
 
           next_an_index <= an_index + 1         after gate_delay;
-          next_state    <= st_wait              after gate_delay;
 
         end if;
 
