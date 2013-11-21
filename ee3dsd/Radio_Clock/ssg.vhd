1abe0957f92691fad9f6d250418da4389b4f40d9
Chris Cummins <chrisc.101@gmail.com>
2013-11-21 13:43:57 +0000

    rc/ssg: Fix vertical alignment

--- ee3dsd/Radio_Clock/ssg.vhd
+++ ee3dsd/Radio_Clock/ssg.vhd
@@ -27,10 +27,9 @@ architecture behav of ssg is
 
   type   states is (st_wait, st_wr0, st_wr1, st_wr2, st_wr3);
 
-  signal state:      states := st_wait;
-  signal next_state: states := st_wait;
-
-  signal wr_sampled: std_logic := '0';
+  signal state:      states                  := st_wait;
+  signal next_state: states                  := st_wait;
+  signal wr_sampled: std_logic               := '0';
   signal di_sampled: byte_vector(3 downto 0) := (others => byte_zero);
 
 begin
@@ -40,9 +39,9 @@ begin
 
     if clk'event and (clk = '1') then
 
-      wr_sampled <= wr after gate_delay;
-      di_sampled <= di after gate_delay;
-      state <= next_state after gate_delay;
+      wr_sampled     <= wr              after gate_delay;
+      di_sampled     <= di              after gate_delay;
+      state          <= next_state      after gate_delay;
 
     end if;
 
@@ -51,41 +50,41 @@ begin
   process (wr_sampled, di_sampled, state)
   begin
 
-    an <= (others => '1') after gate_delay;
+    an               <= (others => '1') after gate_delay;
 
     case state is
 
       when st_wait =>
 
         if (wr_sampled = '1') then
-          next_state <= st_wr0   after gate_delay;
+          next_state <= st_wr0          after gate_delay;
         else
-          next_state <= state    after gate_delay;
+          next_state <= state           after gate_delay;
         end if;
 
       when st_wr0 =>
 
-        an(0)        <= '0'      after gate_delay;
-        ka           <= di_sampled(0)    after gate_delay;
-        next_state   <= st_wr1   after gate_delay;
+        an(0)        <= '0'             after gate_delay;
+        ka           <= di_sampled(0)   after gate_delay;
+        next_state   <= st_wr1          after gate_delay;
 
       when st_wr1 =>
 
-        an(1)        <= '0'      after gate_delay;
-        ka           <= di_sampled(1)    after gate_delay;
-        next_state   <= st_wr2   after gate_delay;
+        an(1)        <= '0'             after gate_delay;
+        ka           <= di_sampled(1)   after gate_delay;
+        next_state   <= st_wr2          after gate_delay;
 
       when st_wr2 =>
 
-        an(2)        <= '0'      after gate_delay;
-        ka           <= di_sampled(2)    after gate_delay;
-        next_state   <= st_wr3   after gate_delay;
+        an(2)        <= '0'             after gate_delay;
+        ka           <= di_sampled(2)   after gate_delay;
+        next_state   <= st_wr3          after gate_delay;
 
       when st_wr3 =>
 
-        an(3)        <= '0'      after gate_delay;
-        ka           <= di_sampled(3)    after gate_delay;
-        next_state   <= st_wait  after gate_delay;
+        an(3)        <= '0'             after gate_delay;
+        ka           <= di_sampled(3)   after gate_delay;
+        next_state   <= st_wait         after gate_delay;
 
     end case;
 
