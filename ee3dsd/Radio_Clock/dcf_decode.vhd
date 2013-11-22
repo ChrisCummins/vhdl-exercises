b394984fe49632fd6431cb14994fc24426531e06
Chris Cummins <chrisc.101@gmail.com>
2013-11-22 23:27:31 +0000

    Import coursework 4 components

---
+++ ee3dsd/Radio_Clock/dcf_decode.vhd
@@ -0,0 +1,42 @@
+library IEEE;
+
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use WORK.util.all;
+
+entity dcf_decode is
+
+    generic
+    (
+        clk_freq:   positive := 125000000; -- Hz
+        gate_delay: time     := 1 ns
+    );
+
+    port
+    (
+        rst:    in  std_logic                    := 'X'; -- reset
+        clk:    in  std_logic                    := 'X'; -- clock
+
+        si:     in  std_logic                    := 'X'; -- start of second in
+        mi:     in  std_logic                    := 'X'; -- start of minute in
+        bi:     in  std_logic                    := 'X'; -- bit in
+        year:   out bcd_digit_vector(3 downto 0) := (3 => bcd_two, 2 => bcd_zero, others => bcd_minus);
+        month:  out bcd_digit_vector(1 downto 0) := (others => bcd_minus);
+        day:    out bcd_digit_vector(1 downto 0) := (others => bcd_minus);
+        hour:   out bcd_digit_vector(1 downto 0) := (others => bcd_minus);
+        minute: out bcd_digit_vector(1 downto 0) := (others => bcd_minus);
+        second: out bcd_digit_vector(1 downto 0) := (others => bcd_zero);
+        tr:     out std_logic                    := '0'  -- new bit trigger
+    );
+
+end dcf_decode;
+
+architecture rtl of dcf_decode is
+
+  -- Your declarations go here --
+
+begin
+
+  -- Your implementation goes here --
+
+end rtl;
---
+++ ee3dsd/Radio_Clock/serdes.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+--library UNISIM;
+
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use WORK.util.all;
+
+entity serdes is
+
+    generic
+    (
+        gate_delay:   time      := 1 ns
+    );
+
+    port
+    (
+        clk_ser:  in  std_logic := 'X';      -- serial clock in
+        clk_par:  in  std_logic := 'X';      -- parallel clock in
+        clk_2par: in  std_logic := 'X';      -- 2 x parallel clock in
+        strobe:   in  std_logic := 'X';      -- strobe
+        data_in:  in  std_logic := 'X';      -- serial data in
+        data_out: out byte      := byte_null -- parallel data out
+   );
+
+end serdes;
+
+architecture behav of serdes is
+
+  -- Your declarations go here --
+
+begin
+
+  -- Your implementation goes here --
+
+end behav;
