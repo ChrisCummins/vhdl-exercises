77238c2c9d8aa8095a9f722d915def39e4297fa0
Chris Cummins <chrisc.101@gmail.com>
2013-11-26 17:51:26 +0000

    rc/dcf_decode: Implement testbench

--- ee3dsd/Radio_Clock/dcf_decode_testbench.vhd
+++ ee3dsd/Radio_Clock/dcf_decode_testbench.vhd
@@ -17,8 +17,9 @@ end dcf_decode_testbench;
 
 architecture tests of dcf_decode_testbench is
 
-  constant test_duration: time      := 58000 ms;
   constant clk_period:    time      := 1000 ms / clk_freq;
+  constant sec_period:    time      := 2 * clk_period;
+  constant test_duration: time      := 62 * sec_period;
   signal end_flag:        std_logic := '0';
 
   signal rst:             std_logic := '0';
@@ -67,7 +68,6 @@ begin
   end process;
 
   process is -- Process to set 'clk'
-    variable clk_var:    std_logic := '0';
   begin
 
     while (end_flag = '0') loop
@@ -77,6 +77,61 @@ begin
       wait for clk_period / 2;
     end loop;
 
+    wait;
+  end process;
+
+  process is -- Process to set 'si'
+  begin
+
+    while (end_flag = '0') loop
+      si <= '1';
+      wait for clk_period;
+      si <= '0';
+      wait for clk_period;
+    end loop;
+
+    wait;
+  end process;
+
+  process is -- Process to set 'mi'
+  begin
+
+    mi <= '1'; -- First minute
+    wait for clk_period;
+    mi <= '0';
+    wait for 60 * sec_period - clk_period;
+    mi <= '1'; -- Second minute
+    wait for clk_period;
+    mi <= '0';
+
+    wait;
+  end process;
+
+  process is -- Process to set 'bi'
+
+    file     data:       text;
+    variable data_line:  line;
+    variable bi_var:     std_logic;
+
+  begin
+
+    file_open(data, "dcf-decode.txt", read_mode);
+
+    while not endfile(data) loop
+
+      readline(data, data_line);
+      read(data_line, bi_var);
+
+      bi <= bi_var;
+      si <= '1';
+      wait for clk_period;
+      bi <= '0';
+      si <= '0';
+      wait for sec_period - clk_period;
+
+    end loop;
+
+    file_close(data);
     wait;
   end process;
 
