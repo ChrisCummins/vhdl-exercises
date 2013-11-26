8efee5faf767d90346f27974b6a2f65ab3b0146f
Chris Cummins <chrisc.101@gmail.com>
2013-11-26 18:35:06 +0000

    rc/dcf_decode: Implement state logic

--- ee3dsd/Radio_Clock/dcf_decode.vhd
+++ ee3dsd/Radio_Clock/dcf_decode.vhd
@@ -33,14 +33,91 @@ end dcf_decode;
 
 architecture rtl of dcf_decode is
 
+  type    states             is (st_wait, st_sample, st_write);
   subtype bit_register       is std_logic_vector(59 downto 0);
   subtype bit_register_index is natural range bit_register'length downto 0;
 
-  signal  reg:    bit_register       := (others => 'X');
-  signal  index:  bit_register_index := 0;
+  signal  si_sampled: std_logic          := '0';
+  signal  mi_sampled: std_logic          := '0';
+  signal  bi_sampled: std_logic          := '0';
+
+  signal  state:      states := st_wait;
+  signal  next_state: states := st_wait;
+
+  signal  reg:        bit_register       := (others => '0');
+  signal  index:      bit_register_index := 0;
+  signal  next_index: bit_register_index := 0;
 
 begin
 
-  -- Your implementation goes here --
+  process(clk, rst)
+  begin
+
+    if (rst = '1') then
+
+      si_sampled <= '0' after gate_delay;
+      mi_sampled <= '0' after gate_delay;
+      bi_sampled <= '0' after gate_delay;
+
+      state <= st_wait after gate_delay;
+      index <= 0 after gate_delay;
+
+    elsif clk'event and (clk = '1') then
+
+      si_sampled <= si after gate_delay;
+      mi_sampled <= mi after gate_delay;
+      bi_sampled <= bi after gate_delay;
+
+      state <= next_state after gate_delay;
+      index <= next_index after gate_delay;
+
+    end if;
+
+  end process;
+
+  process(si_sampled, mi_sampled, bi_sampled, state, index)
+  begin
+
+    tr <= '0' after gate_delay;
+
+    case state is
+
+      when st_wait =>
+
+        if (si_sampled = '1') then
+
+          next_state <= st_sample after gate_delay;
+
+          if (mi_sampled = '1') then
+            next_index <= 0 after gate_delay;
+          else
+            next_index <= index + 1 after gate_delay;
+          end if;
+
+        end if;
+
+      when st_sample =>
+
+        tr <= '1' after gate_delay;
+
+        -- TODO: set second out.
+
+        reg(index) <= bi_sampled after gate_delay;
+
+        if (index = 59) then
+          next_state <= st_write after gate_delay;
+        else
+          next_state <= st_wait after gate_delay;
+        end if;
+
+      when st_write =>
+
+        -- TODO: perform parity checks and write out data.
+
+        next_state <= st_wait after gate_delay;
+
+    end case;
+
+  end process;
 
 end rtl;
