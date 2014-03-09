7d42b717ed880b867887573a55517465c2fa2d6c
Chris Cummins <chrisc.101@gmail.com>
2014-03-09 14:29:32 +0000

    cw3: Add alu_s_do to op process sensitivity list

--- ee4dsa/cw3/execution_unit.vhd
+++ ee4dsa/cw3/execution_unit.vhd
@@ -242,7 +242,7 @@ begin
   -- The instruction set implementation.
   process(rst, rom_data, current_pc, current_icc, current_sr, current_io_out,
           current_sp, current_intr, current_shift, current_ram_index_addr,
-          ram_rdata, io_in, next_reg_b_do, next_reg_c_do) is
+          ram_rdata, io_in, alu_s_do, next_reg_b_do, next_reg_c_do) is
 
     variable load_pc:  program_counter;
     variable stack_pc: program_counter;
