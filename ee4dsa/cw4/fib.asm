4fe360a11d6f2c4b98221085ece381db9b69e836
Chris Cummins <chrisc.101@gmail.com>
2014-03-21 10:07:41 +0000

    cw4: Optimise ISR to use 2 registers

--- ee4dsa/cw4/fib.asm
+++ ee4dsa/cw4/fib.asm
@@ -134,33 +134,29 @@ irq1:
         ;; Preserver registers
         pshr    r10
         pshr    r11
-        pshr    r12
 
         ;; Prepare registers
         ld      r10, sseg_idx   ; r10 = i
         ldih    r11, 0
-        ldih    r12, 0
         ldil    r11, sseg_an_t  ; r11 = an_t
-        ldil    r12, sseg_ka_t  ; r12 = ka_t
         ldd     r11, r11, r10   ; r11 = an_t[i]
-        ldd     r12, r12, r10   ; r12 = ka_t[i]
-
-        ;;  Port writes
         stio    SSEG_AN, r11
+
+        ldil    r11, sseg_ka_t  ; r11 = ka_t
+        ldd     r11, r11, r10   ; r11 = ka_t[i]
         stio    SSEG_KA, r12
 
         ;; Increment index counter
-        ldih    r12, 0
-        ldil    r12, 4          ; r11 = 4
+        ldih    r11, 0
+        ldil    r11, 4          ; r11 = 4
         inc     r10             ; i++
-        lt      r10, r12        ; IF i < 4
+        lt      r10, r11        ; IF i < 4
         brts    irq1_2          ; THEN RETURN
         ldil    r10, 0          ; ELSE i = 0
 irq1_2:
         st      r10, sseg_idx   ; Memory writes
 
         ;; Restore register file
-        popr    r12
         popr    r11
         popr    r10
         reti
