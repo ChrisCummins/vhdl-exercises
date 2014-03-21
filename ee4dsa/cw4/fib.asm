87a0c06ab8d6afa8b1f16ab7bc005a08484ebcfb
Chris Cummins <chrisc.101@gmail.com>
2014-03-21 11:25:43 +0000

    cw4: Sweet ass assembly implementation!

--- ee4dsa/cw4/fib.asm
+++ ee4dsa/cw4/fib.asm
@@ -15,10 +15,9 @@
 
         .dseg
 
-        bcd2sseg_t:     .word 10 ; BCD to SSEG lookup table
-        sseg_idx:       .word 1 ; SSEG refresh index
-        sseg_an_t:      .word 4 ; SSEG Anode values
-        sseg_ka_t:      .word 4 ; SSEG Cathode values
+        ssd_idx:        .word 1 ; SSD refresh index
+        ssd_an_t:       .word 4 ; SSD Anode values
+        ssd_ka_t:       .word 4 ; SSD Cathode values
 
 
         ;; Program code:
@@ -35,30 +34,35 @@
         .def 1000reg    r62
         .def 10000reg   r63
 
-        .def 1digit     r100
-        .def 10digit    r101
-        .def 100digit   r102
-        .def 1000digit  r103
+        .def 1digit     r103
+        .def 10digit    r102
+        .def 100digit   r101
+        .def 1000digit  r100
+
+        .def 1ssd       r107
+        .def 10ssd      r106
+        .def 100ssd     r105
+        .def 1000ssd    r104
 
         ;; Prepare memory and constant registers
 _main:
         cli                     ; Disable interrupts
-        st      NULL, sseg_idx  ; SSEG counter = 0
+        st      NULL, ssd_idx   ; SSD counter = 0
 
-        ;; Setup SSEG tables
+        ;; Setup SSD tables
         ldih    r90,  0
         ldil    r90,  0x0E
-        st      r90,  sseg_an_t
-        st      NULL, sseg_ka_t
+        st      r90,  ssd_an_t
+        st      NULL, ssd_ka_t
         ldil    r90,  0x0D
-        st      r90,  sseg_an_t + 1
-        st      NULL, sseg_ka_t + 1
+        st      r90,  ssd_an_t + 1
+        st      NULL, ssd_ka_t + 1
         ldil    r90,  0x0B
-        st      r90,  sseg_an_t + 2
-        st      NULL, sseg_ka_t + 2
+        st      r90,  ssd_an_t + 2
+        st      NULL, ssd_ka_t + 2
         ldil    r90,  0x07
-        st      r90,  sseg_an_t + 3
-        st      NULL, sseg_ka_t + 3
+        st      r90,  ssd_an_t + 3
+        st      NULL, ssd_ka_t + 3
 
         ;; Setup constants registers
         ldil    10reg,    10
@@ -88,26 +92,38 @@ next_fib:
         pshr    1000reg         ; Denominator
         pshr    sum             ; Numerator
         call    div
-        popr    1digit          ; Thousand digit
+        popr    1000digit       ; Thousand digit
         popr    r35
+        pshr    1000digit       ; BCD 2 SSD
+        call    bcd2ssd
+        popr    1000ssd
 
         pshr    100reg
         pshr    r35
         call    div
-        popr    10digit         ; Hundreds digit
+        popr    100digit        ; Hundreds digit
         popr    r35
+        pshr    100digit        ; BCD 2 SSD
+        call    bcd2ssd
+        popr    100ssd
 
         pshr    10reg
         pshr    r35
         call    div
-        popr    100digit        ; Tens digit
-        popr    1000digit       ; Single digit
+        popr    10digit         ; Tens digit
+        popr    1digit          ; Single digit
+        pshr    10digit         ; BCD 2 SSD
+        call    bcd2ssd
+        popr    10ssd
+        pshr    1digit          ; BCD 2 SSD
+        call    bcd2ssd
+        popr    1ssd
 
-        ;; Write out digits to SSEG table
-        st      1digit,    sseg_ka_t
-        st      10digit,   sseg_ka_t + 1
-        st      100digit,  sseg_ka_t + 2
-        st      1000digit, sseg_ka_t + 3
+        ;; Write out digits to SSD table
+        st      1ssd,    ssd_ka_t
+        st      10ssd,   ssd_ka_t + 1
+        st      100ssd,  ssd_ka_t + 2
+        st      1000ssd, ssd_ka_t + 3
 
         ;; Wait for next request
         call    btnc_press
@@ -136,15 +152,15 @@ isr1:
         pshr    r11
 
         ;; Prepare registers
-        ld      r10, sseg_idx   ; r10 = i
+        ld      r10, ssd_idx    ; r10 = i
         ldih    r11, 0
-        ldil    r11, sseg_an_t  ; r11 = an_t
+        ldil    r11, ssd_an_t   ; r11 = an_t
         ldd     r11, r11, r10   ; r11 = an_t[i]
-        stio    SSEG_AN, r11
+        stio    SSD_AN, r11
 
-        ldil    r11, sseg_ka_t  ; r11 = ka_t
+        ldil    r11, ssd_ka_t   ; r11 = ka_t
         ldd     r11, r11, r10   ; r11 = ka_t[i]
-        stio    SSEG_KA, r12
+        stio    SSD_KA, r11
 
         ;; Increment index counter
         ldih    r11, 0
@@ -154,7 +170,7 @@ isr1:
         brts    isr1_2          ; THEN RETURN
         ldil    r10, 0          ; ELSE i = 0
 isr1_2:
-        st      r10, sseg_idx   ; Memory writes
+        st      r10, ssd_idx    ; Memory writes
 
         ;; Restore register file
         popr    r11
--- ee4dsa/cw4/stdlib.asm
+++ ee4dsa/cw4/stdlib.asm
@@ -31,8 +31,8 @@
 
         ;; Output ports
         .def LEDS       0x00
-        .def SSEG_AN    0x01
-        .def SSEG_KA    0x02
+        .def SSD_AN     0x01
+        .def SSD_KA     0x02
 
         ;; Button masks
         .def BTNU       0x00
@@ -41,6 +41,41 @@
         .def BTNL       0x40
         .def BTNR       0x80
 
+        ;; Hexadecimal encodings for digits oriented abcdefg
+        .def SSD_A2G_0  0x7E
+        .def SSD_A2G_1  0x30
+        .def SSD_A2G_2  0x6D
+        .def SSD_A2G_3  0x79
+        .def SSD_A2G_4  0x33
+        .def SSD_A2G_5  0x5B
+        .def SSD_A2G_6  0x5F
+        .def SSD_A2G_7  0x70
+        .def SSD_A2G_8  0x7F
+        .def SSD_A2G_9  0x7B
+        .def SSD_A2G_A  0x77
+        .def SSD_A2G_B  0x1F
+        .def SSD_A2G_C  0x4E
+        .def SSD_A2G_D  0x3D
+        .def SSD_A2G_E  0x4F
+        .def SSD_A2G_F  0x47
+
+        ;; Hexadecimal encodings for digits oriented gfedcba
+        .def SSD_G2A_0  0x3F
+        .def SSD_G2A_1  0x06
+        .def SSD_G2A_2  0x5B
+        .def SSD_G2A_3  0x4F
+        .def SSD_G2A_4  0x66
+        .def SSD_G2A_5  0x6D
+        .def SSD_G2A_6  0x7D
+        .def SSD_G2A_7  0x07
+        .def SSD_G2A_8  0x7F
+        .def SSD_G2A_9  0x6F
+        .def SSD_G2A_A  0x77
+        .def SSD_G2A_B  0x7C
+        .def SSD_G2A_C  0x39
+        .def SSD_G2A_D  0x5E
+        .def SSD_G2A_E  0x79
+        .def SSD_G2A_F  0x71
 
 ;;; Initialisation.
 ;;; ========================================================
@@ -49,12 +84,13 @@
         .org PROG_START
 
 _stdlib_init:
+        call    _bcd2ssd_init     ; Setup bcd2ssd conversion tables
         jmp     _MAIN_ENTRY_POINT ; Jump to user code entry point
 
 ;;; Subroutines.
 ;;; ========================================================
 
-        ;; System IO Operations.
+        ;; Button operations.
         ;; =================================================
 
         ;; Press and release the up button.
@@ -101,6 +137,63 @@ _btnr_press_2:
         tsti    BUTTONS, BTNR, 0x00
         brts    _btnr_press_2
         ret
+
+
+        ;; Seven Segment Display.
+        ;; =================================================
+
+        .dseg
+
+        _bcd2ssd_t:     .word 16 ; BCD to SSD lookup table
+
+        .cseg
+
+_bcd2ssd_init:
+        ldih    r16, 0
+        ldil    r16, SSD_A2G_0
+        st      r16, _bcd2ssd_t
+        ldil    r16, SSD_A2G_1
+        st      r16, _bcd2ssd_t + 0x1
+        ldil    r16, SSD_A2G_2
+        st      r16, _bcd2ssd_t + 0x2
+        ldil    r16, SSD_A2G_3
+        st      r16, _bcd2ssd_t + 0x3
+        ldil    r16, SSD_A2G_4
+        st      r16, _bcd2ssd_t + 0x4
+        ldil    r16, SSD_A2G_5
+        st      r16, _bcd2ssd_t + 0x5
+        ldil    r16, SSD_A2G_6
+        st      r16, _bcd2ssd_t + 0x6
+        ldil    r16, SSD_A2G_7
+        st      r16, _bcd2ssd_t + 0x7
+        ldil    r16, SSD_A2G_8
+        st      r16, _bcd2ssd_t + 0x8
+        ldil    r16, SSD_A2G_9
+        st      r16, _bcd2ssd_t + 0x9
+        ldil    r16, SSD_A2G_A
+        st      r16, _bcd2ssd_t + 0xA
+        ldil    r16, SSD_A2G_B
+        st      r16, _bcd2ssd_t + 0xB
+        ldil    r16, SSD_A2G_C
+        st      r16, _bcd2ssd_t + 0xC
+        ldil    r16, SSD_A2G_D
+        st      r16, _bcd2ssd_t + 0xD
+        ldil    r16, SSD_A2G_E
+        st      r16, _bcd2ssd_t + 0xE
+        ldil    r16, SSD_A2G_D
+        st      r16, _bcd2ssd_t + 0xF
+        ret
+
+bcd2ssd:
+        popr    r16             ; Return address
+        popr    r17             ; BCD digit 'i'
+        ldih    r18, 0
+        ldil    r18, _bcd2ssd_t ; r18 = bcd2ssd_t
+        ldd     r18, r18, r17   ; r19 = bcd2ss_t[i]
+        pshr    r18             ; Push result
+        pshr    r16             ; Push return address
+        ret
+
 
         ;; Software arithmetic.
         ;; =================================================
