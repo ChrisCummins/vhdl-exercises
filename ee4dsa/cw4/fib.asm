8336a10e632a3d7aefd8a13b17c93bb887c8d49f
Chris Cummins <chrisc.101@gmail.com>
2014-03-25 00:09:41 +0000

    cw4/fib: Implement 10 digit BCD conversion

--- ee4dsa/cw4/fib.asm
+++ ee4dsa/cw4/fib.asm
@@ -15,9 +15,13 @@
 
         .dseg
 
-        ;; Seven segment display memory:
-        ssd_an_t:       .word 4         ; Anode mask table
-        ssd_ka_t:       .word 4         ; Cathode mask table
+bcd_t:    .word UINT_MAX_DIGITS         ; BCD table
+ssd_ka_t: .word UINT_MAX_DIGITS         ; Cathode mask table
+ssd_an_t: .word 4                       ; Anode mask table
+p10_t:    .word 9                       ; Powers of 10 table
+
+msd:      .word 1                       ; Most significant digit
+msd_v:    .word 1                       ; Most significant visible dibit
 
 
         ;; Program code:
@@ -30,10 +34,13 @@
         .def a          r32             ; Fibonacci 'a' operand
         .def b          r33             ; Fibonacci 'b' operand
         .def sum        r34             ; Fibonacci result
-        .def last       r35             ; Last result in Fibonacci sequence
-        .def ssd_ka_r   r36             ; Pointer to SSD cathode table
-        .def led_m      r38             ; LED port mask
-        .def byte+1     r37             ; Byte overflow constant
+        .def msd_r      r35
+        .def ssd_ka_r   r36
+        .def bcd_r      r37
+        .def divisor    r40
+        .def bcd        r41
+        .def last_bcd   r42
+        .def i          r43
 
         ;; The main code entry point. Begin by initialising
         ;; register and memory values as required. We do this
@@ -41,54 +48,100 @@
         ;; be accessing uninitialised memory:
 _main:
         cli                             ; Disable interrupts
-        ldi     ssd_ka_r,     ssd_ka_t  ; Set a pointer to the cathode table
-        ldi     byte+1,       0xFF + 1  ; Set the byte overflow
-        sti     ssd_ka_t,     SSD_OFF   ; Zero the cathode table
-        sti     ssd_ka_t + 1, SSD_OFF
-        sti     ssd_ka_t + 2, SSD_OFF
-        sti     ssd_ka_t + 3, SSD_OFF
-        sti     ssd_an_t,     0x07      ; Anode table with fixed order
+        ldi     bcd_r, bcd_t            ; Set a pointer to the BCD table
+        ldi     ssd_ka_r, ssd_ka_t      ; Set a pointer to the cathode table
+
+        ;; Zero our Anode table
+        sti     ssd_an_t,     0x07
         sti     ssd_an_t + 1, 0x0B
         sti     ssd_an_t + 2, 0x0D
         sti     ssd_an_t + 3, 0x0E
+
+        ;; Zero our Cathode table
+        ldi     i, ssd_ka_t + UINT_MAX_DIGITS - 1
+        stri    i, SSD_OFF
+        dec     i
+        gte     i, ssd_ka_r
+        rbrts   -3
+
+        ;; Set the power of 10 table
+        sti     p10_t,              1   ; 10 & 0
+        sti     p10_t + 1,         10   ; 10 ^ 1
+        sti     p10_t + 2,        100   ; 10 ^ 2
+        sti     p10_t + 3,       1000   ; 10 ^ 3
+        sti     p10_t + 4,      10000   ; 10 ^ 4
+        sti     p10_t + 5,     100000   ; 10 ^ 5
+        sti     p10_t + 6,    1000000   ; 10 ^ 6
+        sti     p10_t + 7,   10000000   ; 10 ^ 7
+        sti     p10_t + 8,  100000000   ; 10 ^ 8
+        sti     p10_t + 9, 1000000000   ; 10 ^ 9
+
         sei                             ; We're all set, so enable interrupts
 
-reset_fib:
-        ldi     a, 0                    ; a = 0
-        ldi     b, 1                    ; b = 1
-        ldi     last, 0
-        ldi     led_m, 1
-next_fib:                               ; The actual Fibonacci calculation:
+fib_init:
+        ldi     a, 4181                 ; a = TODO: Set back to 0
+        ldi     b, 6765                 ; b = TODO: Set back to 1
+
+fib_iter:                               ; The actual Fibonacci calculation:
         add     sum, a, b               ; sum = a + b
         mov     a, b                    ; a = b
         mov     b, sum                  ; b = sum
-        brlt    sum, last, reset_fib    ; Reset if we have integer overflow
-        mov     last, sum               ; last = current
 
         ;; Update Seven Segment Display:
-        pshr    ssd_ka_r                ; Update the SSD cathode table
+        ldil    msd_r, 0               ; Reset msd_r
+        ldi     i, UINT_MAX_DIGITS - 1 ; i = MAX - 1
+
+bcd_loop:
+        lddi    divisor, i, p10_t
+        pshr    divisor                 ; sum /= divisor
         pshr    sum
-        call    bin2ssd_tm
+        call    divu
+        popr    bcd
+        popr    sum
 
-        ;; Update LED indicator
-        stio    LEDS, led_m
-        lsli    led_m, led_m, 1         ; led_m << 1
-        lt      led_m, byte+1           ; If we've overflown the port mask, reset
+        ;; Set msd_r = i + 1 IF bcd > 0 && msd_r = 0
+        eqz     bcd
+        brts    store_bcd
+        nez     msd_r
+        brts    store_bcd
+        mov     msd_r, i                ; msd_r = i
+        inc     msd_r                   ; msd_r = i + 1
+
+store_bcd:                              ; Store BCD digit and store SSD
+        ldd     last_bcd, bcd_r, i      ; last_bcd = bcd[i]
+        equ     bcd, last_bcd           ; IF bcd != bcd[i], then store it
+        brts    next_bcd_loop
+        std     bcd_r, i, bcd           ; bcd[i] = bcd
+        pshr    bcd                     ; Convert bcd to ssd
+        call    bcd2ssd
+        popr    bcd
+        std     ssd_ka_r, i, bcd        ; ssd[i] = ssd
+
+next_bcd_loop:
+        eqz     i                       ; If i = 0, exit loop
+        brts    _wait_for_next
+        dec     i                       ; i--
+        jmp     bcd_loop
+
+_wait_for_next:                         ; Wait for button press
+        gti     msd_r, 4                ; msd_r = max(msd_r, 4)
         rbrts   2
-        ldil    led_m, 1
-
-        ;; Wait for Button press
+        ldil    msd_r, 4
         call    btnc_press
-        jmp     next_fib
+        jmp     fib_iter
 
         ;; Clear up our symbol space:
         .undef a
         .undef b
         .undef sum
-        .undef last
         .undef ssd_ka_r
-        .undef byte+1
-        .undef led_m
+        .undef msd_r
+        .undef ssd_ka_r
+        .undef bcd_r
+        .undef divisor
+        .undef bcd
+        .undef last_bcd
+        .undef i
 
 ;;; Seven Segment Display driver:
 ;;; ========================================================
